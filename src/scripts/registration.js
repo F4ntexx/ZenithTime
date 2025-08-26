b7cff4adba226b0f30506ba3a54767cd1e80776e
Artem Rabets <artemrabets228777@gmail.com>
2025-08-26 20:12:59 +0300

    Registration Dispatch

--- src/scripts/registration.js
+++ src/scripts/registration.js
@@ -1,14 +1,13 @@
-const Form = document.querySelector('form');
-
-Form.addEventListener('submit', (e) =>{
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const formData = new FormData(Form);
-    const data = Object.fromEntries(formData);
-    fetch('https://reqres.in/api/users',{
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        }, 
-        body: JSON.stringify(data)
-    })
+const dataForm = new FormData(form);
+
+fetch('https://httpbin.org/post' , {
+    method: 'POST',
+    body: dataForm
+}).then(Response => Response.json())
+.then(Response => console.log(Response))
+
+window.location.href = "/src/page/app.html"
 })
